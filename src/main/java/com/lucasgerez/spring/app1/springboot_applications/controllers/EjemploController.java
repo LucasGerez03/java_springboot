489f085eade900e0b7d8c63bb2e0e8eda9f0ee19
LucasGerez03 <gerezlucas003@gmail.com>
2026-02-12 16:52:25 -0300

    @ModelAttribute y cambios con thymeleaf

--- src/main/java/com/lucasgerez/spring/app1/springboot_applications/controllers/EjemploController.java
+++ src/main/java/com/lucasgerez/spring/app1/springboot_applications/controllers/EjemploController.java
@@ -1,9 +1,13 @@
 package com.lucasgerez.spring.app1.springboot_applications.controllers;
 
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import com.lucasgerez.spring.app1.springboot_applications.models.Empleados;
 
@@ -15,7 +19,7 @@ public class EjemploController {
     
     
     public String info(Model model){
-        Empleados empleado1 = new Empleados("Lucas", "Gerez", "Calle Falsa 123", "Desarrollador", 2222,111114444, 001);
+        Empleados empleado1 = new Empleados("Lucas", "Gerez", "null_xd", "Desarrollador", 2222,111114444, 001);
 
         model.addAttribute("Titulo", "empleado1_LUCAS");
         model.addAttribute("Empleado", empleado1);
@@ -24,4 +28,14 @@ public class EjemploController {
         return "detalles_info";
     
     } 
+    @ModelAttribute("Empleados")
+    public List<Empleados> ListaEmpleados(){
+        return Arrays.asList(
+            new Empleados("Lucas", "Gerez", "Calle Falsa 123", "Desarrollador", 22,123456789, 001),
+            new Empleados("Cosme", "Fulanito", "Avenida Siempre Viva 456", "Diseñador", 28,987654321, 002),
+            new Empleados("Martina", "Gennari", "Boulevard Central 789", "Gerente", 35,456789123, 003),
+            new Empleados("Ana", "González", "Avenida central no.4", "Diseñadora", 30, 456789123, 04),
+            new Empleados("Pedro", "Sánchez", "Boulevard no.5", "Analista", 25, 789123456, 05)
+        );
+    }
 }

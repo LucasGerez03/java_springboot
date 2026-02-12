d6acd7db2837556f54875e2513b255a89158bac5
LucasGerez03 <gerezlucas003@gmail.com>
2026-02-12 16:16:20 -0300

    clase 10DTO en RestController

--- src/main/java/com/lucasgerez/spring/app1/springboot_applications/controllers/EjemploRestController.java
+++ src/main/java/com/lucasgerez/spring/app1/springboot_applications/controllers/EjemploRestController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 //import org.springframework.web.bind.annotation.RequestParam;
 
 import com.lucasgerez.spring.app1.springboot_applications.models.Empleados;
+import com.lucasgerez.spring.app1.springboot_applications.models.dto.ClaseDTO;
 
 
 
@@ -22,16 +23,22 @@ public class EjemploRestController {
     @GetMapping(path = "/detalles_info2")
     // @RequestMapping(path = "/detalles_info2", method = RequestMethod.GET)
     
+            //Map<String, Object>
+    public ClaseDTO detalles_info(){
+        
+        ClaseDTO usuario1 = new ClaseDTO();
+        usuario1.setTitulo("Administrador");
+        usuario1.setUsuario("Lucas Gerez");
 
-    public Map<String, Object> detalles_info2(){
-        Empleados empleado1 = new Empleados("Lucas", "Gerez", "Calle Falsa 123", "Desarrollador", 22,123456789, 001);
+        
+        /*Empleados empleado1 = new Empleados("Lucas", "Gerez", "Calle Falsa 123", "Desarrollador", 22,123456789, 001);
 
         Map<String, Object> respuesta = new HashMap<>();
         respuesta.put("Empleado", "Datos del Empleado:");
         respuesta.put("Informacion", empleado1);
+        */
         
-        
-        return respuesta; 
+        return usuario1; 
     
     } 
 }

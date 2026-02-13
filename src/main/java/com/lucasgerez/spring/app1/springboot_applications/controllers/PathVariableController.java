d799d929ac2068f70943e92d29c650c8d65ccade
LucasGerez03 <gerezlucas003@gmail.com>
2026-02-12 22:01:04 -0300

    15_@PathVariable, en nuevo controlador

---
+++ src/main/java/com/lucasgerez/spring/app1/springboot_applications/controllers/PathVariableController.java
@@ -0,0 +1,22 @@
+package com.lucasgerez.spring.app1.springboot_applications.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequestMapping("/api/variable")
+public class PathVariableController {
+    @GetMapping("/pagina1/{mensaje}") /* */
+        public ParametroDTO pagina1(@PathVariable String mensaje){
+            
+            ParametroDTO parametro1 = new ParametroDTO();
+            parametro1.setInformacion(mensaje);
+            return parametro1;
+        }
+        
+    }
+    

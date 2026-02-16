1b5c75c9905c1e4a1e8efa980332a856af299cee
LucasGerez03 <gerezlucas003@gmail.com>
2026-02-15 21:24:31 -0300

    @Value

--- src/main/java/com/lucasgerez/spring/app1/springboot_applications/controllers/PathVariableController.java
+++ src/main/java/com/lucasgerez/spring/app1/springboot_applications/controllers/PathVariableController.java
@@ -5,9 +5,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.lucasgerez.spring.app1.springboot_applications.models.Empleados;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -16,6 +19,17 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RestController
 @RequestMapping("/api/variable")
 public class PathVariableController {
+
+    @Value("${config.codigo}")
+    private int codigo;
+    @Value("${config.usuario}")
+    private String usuario;
+    @Value("${config.mensaje}")
+    private String mensaje;
+    @Value("${config.valores}")
+    private String[] valores;
+    
+
     @GetMapping("/pagina1/{mensaje}") /* */
         public ParametroDTO pagina1(@PathVariable String mensaje){
             
@@ -28,10 +42,18 @@ public class PathVariableController {
         public Empleados creaEmple(@RequestBody Empleados empleado1){
             
             
-            
-            
             return empleado1;
         }
-        
-    }
+    
+        @GetMapping("/valores")
+        public Map<String, Object> values(){
+            Map<String, Object> json = new HashMap<>();
+            json.put("codigo", codigo);
+            json.put("usuario", usuario);
+            json.put("mensaje", mensaje);
+            json.put("valores", valores);
+            return json;
+        }
+
+}
     
